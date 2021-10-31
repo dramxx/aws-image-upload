fe846e32f77bbee81092fbd22622498a836df870
dramxx <drahoslav.madar@gmail.com>
2021-10-31 16:41:49 +0100

    Upload image API

--- src/main/java/com/dma/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/dma/awsimageupload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.dma.awsimageupload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -19,4 +21,11 @@ public class UserProfileService {
         return userProfileDataAccessService.getUserProfiles();
     }
 
+    void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+//        TODO:
+//         check file is an image and is not empty
+//         check if user exists
+//         get image metadata,  if applicable
+//         store image in S3 and update fakeDatabase with S3 image url
+    }
 }
